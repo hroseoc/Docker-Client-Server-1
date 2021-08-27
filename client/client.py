7334d4e821ecab9798f25aebd9b703c4b2a0e342
Hannah O'Connor <ocoha990@student.otago.ac.nz>
2021-08-27 12:22:53 +1200

    creating client python file

---
+++ client/client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Imports the python system library.
+import urllib.request
+
+# The req variable contains the request on 'http://localhost:1234/'.
+req = urllib.request.urlopen("http://localhost:1234/")
+
+# 'encodedContent' correspond to the server response encoded ('index.html').
+# 'decodedContent' correspond to the server response decoded (i.e., what we want to display).
+encodedContent = req.read()
+decodedContent = encodedContent.decode("utf8")
+
+# Display the server file: 'index.html'.
+print(decodedContent)
+
+# Close the server connection.
+req.close()
