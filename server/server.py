f98e49674a7a92ec81da7a27b79ac8071c2a638a
Hannah O'Connor <ocoha990@student.otago.ac.nz>
2021-08-27 12:27:06 +1200

    creating server python file

---
+++ server/server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Import of python system libraries (these libraries will be used to create the web server)
+import http.server
+import socketserver 
+
+# This variable handles the requests from client on the server.
+handle = http.server.SimpleHTTPRequestHandler
+
+# Here we define that we want to start the server on port 1234. 
+# Try to remember this information it will be very useful to us later with docker-compose.
+with socketserver.TCPServer(("", 1234), handle) as httpd:
+    # This instruction will keep the server running, waiting for requests from the client.
+    httpd.serve_forever()
