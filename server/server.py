194993b3ec165a54ca713dfbce8efadd83205a01
Hannah O'Connor <ocoha990@student.otago.ac.nz>
2021-08-27 13:15:10 +1200

    corrected port number

--- server/server.py
+++ server/server.py
@@ -9,6 +9,6 @@ handle = http.server.SimpleHTTPRequestHandler
 
 # Here we define that we want to start the server on port 1234. 
 # Try to remember this information it will be very useful to us later with docker-compose.
-with socketserver.TCPServer(("", 1234), handle) as httpd:
+with socketserver.TCPServer(("", 3000), handle) as httpd:
     # This instruction will keep the server running, waiting for requests from the client.
     httpd.serve_forever()
